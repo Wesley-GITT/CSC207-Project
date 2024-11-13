ada7e5a463bf9aa907d98ee025d9c5b78432bbff
Juntongdalao <eric.superking.6626@gmail.com>
2024-11-13 14:50:58 -0500

    nov13-1

--- src/use_case/signup/SignUpInputBoundary.java
+++ src/use_case/signup/SignUpInputBoundary.java
@@ -4,5 +4,14 @@ package use_case.signup;
 Interface to define the input for the sign-up process
  */
 public interface SignUpInputBoundary {
+    /**
+     * Register user the signup use case.
+     * @param signUpInputData the input data
+     */
     SignUpOutputData registerUser(SignUpInputData signUpInputData);
+
+    /**
+     * Executes the switch to login view use case.
+     */
+    void switchToLoginView();
 }
