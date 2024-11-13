9fe21dd95ba92ff160ce619a3e05d6366817ae4b
Juntongdalao <eric.superking.6626@gmail.com>
2024-11-13 14:50:58 -0500

    nov13-1

--- src/use_case/login/LoginInputBoundary.java
+++ src/use_case/login/LoginInputBoundary.java
@@ -4,5 +4,9 @@ package use_case.login;
 Interface to define of the process of login
  */
 public interface LoginInputBoundary {
+    /**
+     * Executes the login use case.
+     * @param loginInputData the input data
+     */
     LoginOutputData login(LoginInputData loginInputData);
 }
