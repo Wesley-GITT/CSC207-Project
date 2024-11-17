6473e685d4acf5e4c443eb6b59be42acae7c7396
Wes-Wei <jing.wei@mail.utoronto.ca>
2024-11-16 22:36:02 -0500

    Update Book Entities

--- src/main/java/entity/BookFactory.java
+++ src/main/java/entity/BookFactory.java
@@ -5,8 +5,7 @@ import java.util.Set;
 public class BookFactory {
 
     public Book create(
-            String id, String imageUrl, String title, Set<String> authorIds, Set<String> translatorIds, String lang,
-            String publishers, String description, String sampleUrl) {
-        return new Book(id, imageUrl, title, authorIds, translatorIds, lang, publishers, description, sampleUrl);
+            String id, String imageUrl, String title, String publisher, String publishedDate, String description, String sampleUrl, String language, Set<String> authors, Set<String> categories) {
+        return new Book(id, imageUrl, title, publisher, publishedDate, description, sampleUrl, language, authors, categories);
     }
 }
