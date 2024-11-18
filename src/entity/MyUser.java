7b507704334b0bca4e25d445e3a2e0c4f679edf1
Juntongdalao <eric.superking.6626@gmail.com>
2024-11-18 11:47:44 -0500

    Nov18-build Clean Architexture

--- src/entity/MyUser.java
+++ src/entity/MyUser.java
@@ -1,18 +1,21 @@
 package entity;
 
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 
-public class MyUser extends OtherUser implements User {
+public class MyUser extends OtherUser {
 
     private String password;
     private String address;
     private Set<Integer> cartItems;
 
-    public MyUser(String username, String password, String address, ContactInfo contactInfo, Set<Integer> cartItems, Set<Integer> productIds) {
-        super(username, contactInfo, productIds);
+    public MyUser(int id, String username, String password, String address, ContactInfo contactInfo, Set<Integer> cartItems) {
+        super(id, username, contactInfo);
         this.password = password;
         this.address = address;
-        this.cartItems = cartItems;
+        this.cartItems = new HashSet<>();
+        this.setCartItems(cartItems);
     }
 
     public void setUsername(String username) {
@@ -43,19 +46,27 @@ public class MyUser extends OtherUser implements User {
         this.contactInfo.setTelephone(telephone);
     }
 
-    public void addCartItem(int productId) {
-        this.cartItems.add(productId);
+    public Set<Integer> getCartItems() {
+        // don't allow code of other scope to modify cartItems without using setter
+        return Set.copyOf(this.cartItems);
     }
 
-    public void removeCartItem(int productId) {
-        this.cartItems.remove(productId);
-    }
+    public void setCartItems(Set<Integer> cartItems) {
+        // add and remove manually to save memory for copying
+        Iterator<Integer> selfCartItemsIterator = this.cartItems.iterator();
+        while (selfCartItemsIterator.hasNext()) {
+            Integer cartItem = selfCartItemsIterator.next();
+            if (!cartItems.contains(cartItem)) {
+                selfCartItemsIterator.remove();
+            }
+        }
 
-    public void addProduct(int productId) {
-        this.productIds.add(productId);
-    }
-
-    public void removeProduct(int productId) {
-        this.productIds.remove(productId);
+        Iterator<Integer> resultantCartItemsIterator = cartItems.iterator();
+        while (resultantCartItemsIterator.hasNext()) {
+            Integer cartItem = resultantCartItemsIterator.next();
+            if (!this.cartItems.contains(cartItem)) {
+                this.cartItems.add(cartItem);
+            }
+        }
     }
 }
