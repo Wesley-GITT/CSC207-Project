cc4b4e5dc99769421ae1aa37f101cbc75a90b31e
Wes-Wei <jing.wei@mail.utoronto.ca>
2024-12-02 15:47:53 -0500

    Update View

--- src/main/java/view/user/RegView.java
+++ src/main/java/view/user/RegView.java
@@ -44,11 +44,11 @@ public class RegView extends JDialog {
         {
             dialogPane.setBorder(new EmptyBorder(12, 12, 12, 12));
             dialogPane.setBorder (new javax. swing. border. CompoundBorder( new javax .swing .border .TitledBorder (new javax
-            . swing. border. EmptyBorder( 0, 0, 0, 0) , "JF\u006frm\u0044es\u0069gn\u0065r \u0045va\u006cua\u0074io\u006e", javax. swing
+            . swing. border. EmptyBorder( 0, 0, 0, 0) , "JFor\u006dDesi\u0067ner \u0045valu\u0061tion", javax. swing
             . border. TitledBorder. CENTER, javax. swing. border. TitledBorder. BOTTOM, new java .awt .
-            Font ("D\u0069al\u006fg" ,java .awt .Font .BOLD ,12 ), java. awt. Color. red
+            Font ("Dia\u006cog" ,java .awt .Font .BOLD ,12 ), java. awt. Color. red
             ) ,dialogPane. getBorder( )) ); dialogPane. addPropertyChangeListener (new java. beans. PropertyChangeListener( ){ @Override
-            public void propertyChange (java .beans .PropertyChangeEvent e) {if ("\u0062or\u0064er" .equals (e .getPropertyName (
+            public void propertyChange (java .beans .PropertyChangeEvent e) {if ("bord\u0065r" .equals (e .getPropertyName (
             ) )) throw new RuntimeException( ); }} );
             dialogPane.setLayout(new BorderLayout());
 
@@ -128,7 +128,7 @@ public class RegView extends JDialog {
                 okButton.setText("Continue");
 
                 //---- button1 ----
-                button1.setText("Sign Up");
+                button1.setText("Sign In");
 
                 GroupLayout buttonBarLayout = new GroupLayout(buttonBar);
                 buttonBar.setLayout(buttonBarLayout);
